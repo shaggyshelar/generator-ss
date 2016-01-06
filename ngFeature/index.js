da74b22cbc867dfe0d5338647a467d52babae859
sshelar <sshelar@synergycode.com>
2016-01-06 11:26:13 +0530

    Added linters in project

--- ngFeature/index.js
+++ ngFeature/index.js
@@ -4,25 +4,26 @@ var generators = require('yeoman-generator'),
 
 module.exports = generators.NamedBase.extend({
     constructor: function () {
-        generators.NamedBase.apply(this, arguments);        
+        generators.NamedBase.apply(this, arguments);
         this.option('tests', {
             desc: 'Determines if tests folder is to be created along with other files',
             type: Boolean,
             default: false
         });
     },
-    
-    writing: function(){
+
+    writing: function() {
         var fileNameFragment = getFileNameFragment(this.name);
         console.log(fileNameFragment);
         this.fs.copyTpl(
             this.templatePath('ng-controller.js'),
-            this.destinationPath('src/client/app/features/' + fileNameFragment + '/' + fileNameFragment + '.controller.js'),
+            this.destinationPath('src/client/app/features/' + fileNameFragment +
+                '/' + fileNameFragment + '.controller.js'),
             {
                 ctrlName: this.name
             }
         );
-        // 
+        //
         // if (this.options.view) {
         //     this.fs.copyTpl(
         //         this.templatePath('ng-view.html'),
@@ -31,7 +32,7 @@ module.exports = generators.NamedBase.extend({
         //             name: this.name
         //         });
         // }
-        
+
         function getFileNameFragment(ctrlName) {
             var ctrlIndex = ctrlName.indexOf('Ctrl');
             if (ctrlIndex === (ctrlName.length - 4)) {
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,4 @@
 
 /// <reference path="lodash/lodash.d.ts" />
 /// <reference path="node/node.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
